9eb84418ca337684f57f83905c201507b28a01e8
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-03-19 15:48:00 +0530

    created translate file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template 
-
+from flask import Flask, render_template, request
+from translate import *
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,5 +7,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/", methods=["POST", "GET"])
+def main():
+    if request.method == "POST":
+        input = request.form.get("input_text")
+        selected_lang = request.form.get("selected_language", None)
+        if selected_lang is not None:
+            lang = selected_lang
+        return render_template("index.html", input=input, lang=lang)        
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5000, host='0.0.0.0')
---
+++ translate.py
@@ -0,0 +1,10 @@
+import boto3
+
+
+def do_translate(input):
+    translate = boto3.client(service_name="translate", region_name="us-east-1",use_ssl=True)
+    result = translate.translate_text(Text=input, SourceLanguageCode="en", TargetLanguageCode="es")
+    return result.get("TranslatedText")
+
+a = "Hello World"
+print(do_translate(a))
